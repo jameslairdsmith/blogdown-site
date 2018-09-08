2aa5dd78d54856258aef9320cdc497f88785d52c
James Laird-Smith <jameslairdsmith@gmail.com>
2018-09-08 17:51:48 +0100

    Added rainfall d3.js graphic.

--- content/post/rainfallBars.js
+++ content/post/rainfallBars.js
@@ -1,4 +1,4 @@
-// !preview r2d3 data=read_csv("https://raw.githubusercontent.com/jameslairdsmith/Home_and_dry/master/Rained_out.csv")
+
 //
 //
 
@@ -8,49 +8,49 @@ var width = Math.min(width, totalWidth*0.8);
 
 var margin = ({top: 20, right: 0, bottom: 30, left: 40});
 
-var x = d3.scaleBand()
-    .domain(data.map(d => d.year))
-    .range([margin.left, width - margin.right])
-    .padding(0.1);
+var parseTime = d3.timeParse("%Y-%m-%d");
+
+var dataLength = function(d) {return d.length};
+
+var barWidth = (width*0.9)/dataLength(data);
+
+customDateTicks = width * 0.015
+
+
+var x = d3.scaleTime()
+    .domain(d3.extent(data, d => parseTime(d.date)))
+    .range([margin.left, width - margin.right]);
     
 var y = d3.scaleLinear()
     .domain([0, d3.max(data, d => d.prcp)]).nice()
     .range([height - margin.bottom, margin.top]);
     
+        
 var xAxis = g => g
-    .attr("transform", `translate(0,${height - margin.bottom})`)
-    .call(d3.axisBottom(x)
-        .tickSizeOuter(0))
+  .attr("transform", `translate(${barWidth/2},${height - margin.bottom + margin.bottom*0.2})`)
+  .call(d3.axisBottom(x).tickSizeOuter(0).tickSize(0).ticks(customDateTicks))
+  .call(g => g.select(".domain").remove());
         
 var yAxis = g => g
-    .attr("transform", `translate(${margin.left},0)`)
+    .attr("transform", `translate(${margin.left - margin.left*0.2},0)`)
     .call(d3.axisLeft(y))
-    .call(g => g.select(".domain").remove())
+    .call(g => g.select(".domain").remove());
 
 svg.append("g")
       .attr("fill", "steelblue")
     .selectAll("rect").data(data).enter().append("rect")
-      .attr("x", d => x(d.year))
+      .attr("x", d => x(parseTime(d.date)))
       .attr("y", d => y(d.prcp))
       .attr("height", d => y(0) - y(d.prcp))
-      .attr("width", x.bandwidth());
+      .attr("width", barWidth);
   
 svg.append("g")
       .call(xAxis);
   
 svg.append("g")
-      .call(yAxis)
+      .call(yAxis);
 
 
-svg.selectAll('rect')
-  .data(data)
-  .enter()
-    .append('rect')
-      .attr('width', function(d) { return d.prcp * 1; })
-      .attr('height', '20px')
-      .attr('y', function(d, i) { return i * 22; })
-      .attr('fill', "blue");
-      
 var test = svg.append("g");
 
 test.append('text')
