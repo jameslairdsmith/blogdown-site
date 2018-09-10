4c15a4ad73c03eb4ae5acaa8d64e5f7e87ccd37e
James Laird-Smith <jameslairdsmith@gmail.com>
2018-09-10 21:18:43 +0100

    Changes.

--- content/post/rainfallBars.js
+++ content/post/rainfallBars.js
@@ -1,4 +1,50 @@
-//
+
+function wrap(text, width) {
+  text.each(function() {
+    var text = d3.select(this),
+        words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1, // ems
+        y = text.attr("y"),
+        dy = parseFloat(text.attr("dy")),
+        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+    while (word = words.pop()) {
+      line.push(word);
+      tspan.text(line.join(" "));
+      if (tspan.node().getComputedTextLength() > width) {
+        line.pop();
+        tspan.text(line.join(" "));
+        line = [word];
+        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+      }
+    }
+  });
+}
+
+var title = svg.append("g")
+      .attr('class','head2')
+      .append('text')
+      //.attr("transform", "translate(0,25)")
+      //.attr("y",25)
+      .attr("dy", "1")
+      .text("Rainfall at Cape Town International Airport")
+      .call(wrap, width);
+      
+//title.attr("y",title.node().getBBox().height)
+//    .attr("x",0)
+//    .attr("text-anchor", "start");
+
+//title.attr("y",25);
+    
+    
+var subtitle = svg.append("g").attr('class','axisRed').append('text')
+      .text("Percent difference from ten year average");
+      
+subtitle.attr("y",subtitle.node().getBBox().height + title.node().getBBox().height*1.1)
+    .attr("x",0)
+    .attr("text-anchor", "start");
 
 //
 
@@ -10,10 +56,12 @@ var remainder = totalWidth - width;
 
 marginSpace = Math.max(0.1 * width, remainder);
 
-var excess = Math.max(0.2 * width, width - 500);
+var excess = Math.max(0.25 * width, width - 500);
+
+
 
 var margin = ({
-  top: 20, 
+  top: 20 + title.node().getBBox().height*1.1 + subtitle.node().getBBox().height*1.1, 
   right: excess/2, 
   bottom: 30, 
   left: excess/2
@@ -32,7 +80,7 @@ var x = d3.scaleTime()
     .range([margin.left, width - margin.right]);
     
 var y = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.prcp)]).nice()
+    .domain([d3.min(data, d => d.avrPrcpPct), d3.max(data, d => d.avrPrcpPct)]).nice()
     .range([height - margin.bottom, margin.top]);
     
         
@@ -45,13 +93,20 @@ var yAxis = g => g
     .attr("transform", `translate(${margin.left - margin.left*0.2},0)`)
     .call(d3.axisLeft(y))
     .call(g => g.select(".domain").remove());
+    
+chooseBars = function(d){
+  if(y(d.avrPrcpPct) < y(0))
+  {return y(d.avrPrcpPct)}
+  else {return y(0)}
+};
 
 svg.append("g")
       .attr("fill", "steelblue")
     .selectAll("rect").data(data).enter().append("rect")
       .attr("x", d => x(parseTime(d.date)))
-      .attr("y", d => y(d.prcp))
-      .attr("height", d => y(0) - y(d.prcp))
+      //.attr("y", d => y(d.avrPrcpPct))
+      .attr("y", chooseBars)
+      .attr("height", d => Math.abs(y(0)-y(d.avrPrcpPct)))
       .attr("width", barWidth);
   
 svg.append("g").attr("class", "axisRed")
@@ -60,20 +115,31 @@ svg.append("g").attr("class", "axisRed")
 svg.append("g").attr("class", "axisRed")
       .call(yAxis);
       
+      
 
-var title = svg.append("g").attr('class','h1').append('text')
-    .text("A Rainfall Title");
-    
-title.attr("y",title.node().getBBox().height)
-    .attr("x",5)
-    .attr("text-anchor", "start");
-    
-var dims = title.node().getBBox();
+      
 
-var test = svg.append("g");
+//var title = svg.append("g").attr('class','h1').append('text')
+//    .text("A Rainfall Title");
+
+//title.attr("y",title.node().getBBox().height)
+//    .attr("x",5)
+//    .attr("text-anchor", "start");
+
+
+//var title = svg.append("g").append('h2').text("A Rainfall Title")
+//    .attr("y",10)
+//    .attr("x",10)
+//    .attr("text-anchor", "start");
+    
+
+    
+//var dims = title.node().getBBox();
+
+var test = svg.append("g").attr('class','footnotes');
 
 test.append('text')
-  .text(dims.height)
+  .text(width)
   .attr("y", height)
   .attr("x", width)
   .attr("text-anchor", "end");
