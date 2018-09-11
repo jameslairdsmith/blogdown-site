e55ac4f7e528c5ce5dbce7bc60cacb1ded3b0e21
James Laird-Smith <jameslairdsmith@gmail.com>
2018-09-11 13:10:03 +0100

    Changes to post chart.

--- content/post/rainfallBars.js
+++ content/post/rainfallBars.js
@@ -1,4 +1,6 @@
 
+// As part of the r2d3 package setup, the width and height properties are already specified.
+
 function wrap(text, width) {
   text.each(function() {
     var text = d3.select(this),
@@ -6,7 +8,7 @@ function wrap(text, width) {
         word,
         line = [],
         lineNumber = 0,
-        lineHeight = 1, // ems
+        lineHeight = 0.2, // ems
         y = text.attr("y"),
         dy = parseFloat(text.attr("dy")),
         tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
@@ -24,7 +26,7 @@ function wrap(text, width) {
 }
 
 var title = svg.append("g")
-      .attr('class','head2')
+      .attr('class','ChartHeading')
       .append('text')
       //.attr("transform", "translate(0,25)")
       //.attr("y",25)
@@ -32,23 +34,25 @@ var title = svg.append("g")
       .text("Rainfall at Cape Town International Airport")
       .call(wrap, width);
       
-//title.attr("y",title.node().getBBox().height)
-//    .attr("x",0)
-//    .attr("text-anchor", "start");
-
-//title.attr("y",25);
-    
-    
-var subtitle = svg.append("g").attr('class','axisRed').append('text')
-      .text("Percent difference from ten year average");
+var subtitle = svg.append("g").attr('class','ChartSubHeading').append('text')
+      .attr("dy", "1")
+      .text("Percentage difference from ten year average");
       
 subtitle.attr("y",subtitle.node().getBBox().height + title.node().getBBox().height*1.1)
-    .attr("x",0)
+    //.attr("x",0)
     .attr("text-anchor", "start");
 
-//
+var source = svg.append("g").attr('class','ChartSubHeading');
 
-// width already exists.
+var source = source.append('text')
+  .text("Source: ncdc.noaa.gov")
+  .attr("y", height)
+  //.attr("x", width)
+  .attr("dominant-baseline","ideographic")
+  //.attr("dy",-3)
+  //.attr("padding-right","100%")
+  .attr("text-anchor", "end");
+  
 
 var totalWidth = document.getElementsByTagName('body')[0].offsetWidth;
 
@@ -56,16 +60,38 @@ var remainder = totalWidth - width;
 
 marginSpace = Math.max(0.1 * width, remainder);
 
-var excess = Math.max(0.25 * width, width - 500);
+chooseExcess = function(givenWidth,maxwidth){
+  if(givenWidth < maxwidth)
+  {return 0.25*givenWidth}
+  else {return givenWidth-maxwidth}
+};
 
+//var excess = Math.max(0.25 * width, width - 1000);
 
+var excess = chooseExcess(width,400);
 
 var margin = ({
-  top: 20 + title.node().getBBox().height*1.1 + subtitle.node().getBBox().height*1.1, 
-  right: excess/2, 
-  bottom: 30, 
+  top: 20 + title.node().getBBox().height*1.1 + subtitle.node().getBBox().height*1.2, 
+  right: excess/2 * 1.2, 
+  bottom: 30 + source.node().getBBox().height*1.2,
   left: excess/2
   });
+  
+findHeadingMargin = function(givenWidth,maxWidth){
+  if(givenWidth < maxWidth)
+  {return 0}
+  else {return margin.left*0.6}
+};
+
+var headingMargin = findHeadingMargin(width,400);
+  
+title.attr("transform",`translate(${headingMargin},0)`);
+
+subtitle.attr("transform",`translate(${headingMargin},0)`);
+
+//source.attr("transform",`translate(${width - margin.right},${height})`);
+
+
 
 var parseTime = d3.timeParse("%Y-%m-%d");
 
@@ -73,7 +99,20 @@ var dataLength = function(d) {return d.length};
 
 var barWidth = ((width - excess)*0.8)/dataLength(data);
 
+source.attr("x",width - margin.right + barWidth);
+
 var customDateTicks = width * 0.015;
+
+//var customTimeFormat = d3.time.format.multi([
+//  ["%YY", function(d) {return if(d.getFullYear()==2007){return d.getFullYear()}{return d.getFullYear().slice//(-2)}}],
+//]);
+
+var customTimeFormat = function(d){
+  if(d.getFullYear()==2007)
+  {return d3.timeFormat("%Y")(d)}
+  else {return d3.timeFormat("%y")(d)};
+};
+    
 
 var x = d3.scaleTime()
     .domain(d3.extent(data, d => parseTime(d.date)))
@@ -86,12 +125,20 @@ var y = d3.scaleLinear()
         
 var xAxis = g => g
   .attr("transform", `translate(${barWidth/2},${height - margin.bottom + margin.bottom*0.2})`)
-  .call(d3.axisBottom(x).tickSizeOuter(0).tickSize(0).ticks(customDateTicks))
+  .call(d3.axisBottom(x)
+    .tickSizeOuter(0)
+    .tickSize(0)
+    //.ticks(customDateTicks)
+    .tickFormat(customTimeFormat))
+    //.tickFormat(d3.timeFormat("%y")))
   .call(g => g.select(".domain").remove());
         
 var yAxis = g => g
     .attr("transform", `translate(${margin.left - margin.left*0.2},0)`)
-    .call(d3.axisLeft(y))
+    .call(d3.axisLeft(y)
+      .ticks(6)
+      .tickSize(-width + excess - barWidth)
+      )
     .call(g => g.select(".domain").remove());
     
 chooseBars = function(d){
@@ -101,7 +148,7 @@ chooseBars = function(d){
 };
 
 svg.append("g")
-      .attr("fill", "steelblue")
+      .attr("fill", "grey")
     .selectAll("rect").data(data).enter().append("rect")
       .attr("x", d => x(parseTime(d.date)))
       //.attr("y", d => y(d.avrPrcpPct))
@@ -109,10 +156,10 @@ svg.append("g")
       .attr("height", d => Math.abs(y(0)-y(d.avrPrcpPct)))
       .attr("width", barWidth);
   
-svg.append("g").attr("class", "axisRed")
+svg.append("g").attr("class", "AxisStyle")
       .call(xAxis);
   
-svg.append("g").attr("class", "axisRed")
+svg.append("g").attr("class", "AxisStyle")
       .call(yAxis);
       
       
@@ -136,10 +183,3 @@ svg.append("g").attr("class", "axisRed")
     
 //var dims = title.node().getBBox();
 
-var test = svg.append("g").attr('class','footnotes');
-
-test.append('text')
-  .text(width)
-  .attr("y", height)
-  .attr("x", width)
-  .attr("text-anchor", "end");
