68cd54c8c12dc23fa802b9f751e27c85475f9474
James Laird-Smith <jameslairdsmith@gmail.com>
2018-09-19 17:43:55 +0100

    Changes to chart margin.

--- content/post/damLevelBars.js
+++ content/post/damLevelBars.js
@@ -78,7 +78,7 @@ var svg = div.append("svg")
 // ------ Setting the margins ---------------//
 {
   var margin = ({
-  top: 0.05 * height, 
+  top: 0.03 * height, 
   right: 0.02 * workingWidth, 
   bottom: 0.02 * height,
   left: 0.02 * workingWidth
@@ -96,7 +96,7 @@ var svg = div.append("svg")
       .append('text')
       .attr("dy",0)   // must be specified for the text wrapping function to work.
       .text("Western Cape dam levels")
-      .call(wrap, workingWidth - margin.left - margin.right)
+      //.call(wrap, workingWidth - margin.left - margin.right)
       .attr("transform", `translate(${margin.left},${margin.top})`)
       .attr("dominant-baseline","hanging");
 }
